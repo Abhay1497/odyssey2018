52b6924dec36067cff54ecf424f4d16f482d0778
Vivek Pavaskar <vivek.pavaskar@gmail.com>
2018-03-23 19:12:31 +0530

    database connection bug fixed

--- reg.php
+++ reg.php
@@ -1,68 +1,59 @@
 <?php
-
-$ev=$n=$em=$m=$c=$gn="";
+/**
+ * Created by PhpStorm.
+ * User: Vivek
+ * Date: 23-03-2018
+ * Time: 06:00 PM
+ */
+$event_id = $name = $email = $mobile = $college = $members_names = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	
-	if(!empty($_POST['event'])){
-		$ev=$_POST['event'];
-		echo $ev;
-	}
-	else{
-		echo "empty";
-	}
 
-	if(!empty($_POST['name'])){
-		$n=$_POST['name'];
-		echo $n;
-	}
-	else{
-		echo "empty";
-	}
+    if (!empty($_POST['event'])) {
+        $event_id = $_POST['event'];
+//        echo $event_id;
+    }
 
-	if(!empty($_POST['email'])){
-		$em=$_POST['email'];
-		echo $em;
-	}
-	else{
-		echo "empty";
-	}
+    if (!empty($_POST['name'])) {
+        $name = $_POST['name'];
+//        echo $name;
+    }
 
-	if(!empty($_POST['mobile'])){
-		$m=$_POST['mobile'];
-		echo $m;
-	}
-	else{
-		echo "empty";
-	}
+    if (!empty($_POST['email'])) {
+        $email = $_POST['email'];
+//        echo $email;
+    }
 
-	if(!empty($_POST['coll'])){
-		$c=$_POST['coll'];
-		echo $c;
-	}
-	else{
-		echo "empty";
-	}
+    if (!empty($_POST['mobile'])) {
+        $mobile = $_POST['mobile'];
+//        echo $mobile;
+    }
 
-	if(!empty($_POST['members'])){
-		$gn=$_POST['members'];
-		echo $gn;
-	}
-	else{
-		echo "empty";
-	}
+    if (!empty($_POST['coll'])) {
+        $college = $_POST['coll'];
+//        echo $college;
+    }
 
-$conn = mysqli_connect("localhost","root","","odyssey18");
+    if (!empty($_POST['members'])) {
+        $members_names = $_POST['members'];
+//        echo $members_names;
+    }
 
-if ($conn) {
-	echo "successfull";
-	$sql="insert into register (eid,mobile,name,college,email,memberNames) values('$ev','$m','$n','$c','$em','$gn')";
-	$result=mysqli_query($conn,$sql);
-	echo $result;
-}else{
-	echo "unsuccessfull";
-}
+    $conn = mysqli_connect("localhost", "root", "", "odyssey18");
+
+    if ($conn) {
+        echo "successfull";
+        if ($name == "" and $mobile == "" and $email == "" and $college == "" and $members_names == "") {
+            echo "Please enter a valid information into respective fields.";
+        }else{
+            $sql = "insert into registration (event_id,mobile,name,college,email,member_names) values('$event_id','$mobile','$name','$college','$email','$members_names')";
+            $result = mysqli_query($conn, $sql);
+            echo "Registered Successfully";
+        }
+    } else {
+        echo "Sorry for inconvience, please try again later.";
+    }
+    $conn->close();
 
 }
-
 ?>
