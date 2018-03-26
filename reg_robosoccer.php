25e4579bbf7de6f18a1a1d3725c2e7a6dc5f1dd2
Vivek Pavaskar <vivek.pavaskar@gmail.com>
2018-03-26 15:21:27 +0530

    Robo soccer html and php created

---
+++ reg_robosoccer.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vivek
+ * Date: 26-03-2018
+ * Time: 03:17 PM
+ */
+
+$name1 = $email1 = $mobile1 = $name2 = $email2 = $mobile2 = $college = "";
+$event_id = "ODEC01-";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    if (!empty($_POST['name1'])) {
+        $name1 = $_POST['name1'];
+    }
+
+    if (!empty($_POST['email1'])) {
+        $email1 = $_POST['email1'];
+    }
+
+    if (!empty($_POST['mobile1'])) {
+        $mobile1 = $_POST['mobile1'];
+    }
+
+    if (!empty($_POST['name2'])) {
+        $name2 = $_POST['name2'];
+    }
+
+    if (!empty($_POST['email2'])) {
+        $email2 = $_POST['email2'];
+    }
+
+    if (!empty($_POST['mobile2'])) {
+        $mobile2 = $_POST['mobile2'];
+    }
+
+    if (!empty($_POST['college'])) {
+        $college = $_POST['college'];
+    }
+
+
+    $conn = mysqli_connect("localhost", "root", "", "odyssey2k18");
+
+    if ($conn) {
+
+        $sql = "SELECT max(part_id) FROM robosoccer";
+        $retval = mysqli_query($conn, $sql);
+        if(!$retval)
+        {
+            die('could not get data:'.mysqli_error());
+        }
+
+        while($row=mysqli_fetch_array($retval,MYSQLI_ASSOC))
+        {
+            $id=$row['max(part_id)'];
+            $id=$id+1;
+
+        }
+        $event_id=$event_id.$id;
+        $sql = "insert into robosoccer values('$name1','$mobile1','$email1','$name2','$mobile2','$email2','$college','$id')";
+        if (mysqli_query($conn, $sql) == 1) {
+
+            //sending email
+            $email_subject = "ODYSSEY Registration";
+            $email_body = "Thank you for registering in Robo-Soccer.\nYour Participation ID is ".$event_id;
+            mail($email1, $email_subject, $email_body);
+            mail($email2, $email_subject, $email_body);
+
+            //sending sms
+            include('way2sms-api.php');
+            sendWay2SMS ( "8197508688","R3694K",$mobile1,$email_body);
+            sendWay2SMS ( "8197508688","R3694K",$mobile2,$email_body);
+
+            ?>
+            <script language="javascript" type="text/javascript">
+                alert('Registered successfully.Your ID is <?php echo $event_id;?>');
+                window.location = "reg_robosoccer.html";
+            </script>
+            <?php
+            $conn->close();
+        } else {
+            echo "<br>You have already registered for this event.";
+        }
+    }
+} else {
+    echo "Sorry for inconvience, please try again later.";
+}
+
+
+?>
