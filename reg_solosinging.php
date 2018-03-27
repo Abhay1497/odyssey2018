6acf5e6089c4435c93691627bcfca011309a858b
Vivek Pavaskar <vivek.pavaskar@gmail.com>
2018-03-27 15:05:03 +0530

    solosinging, takeshiscastle, treasurehunt, ultimoingenerio completed

---
+++ reg_solosinging.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vivek
+ * Date: 23-03-2018
+ * Time: 06:00 PM
+ */
+$name = $email = $mobile = $college = "";
+$event_id = "ODCLSS-";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    if (!empty($_POST['name'])) {
+        $name = $_POST['name'];
+    }
+
+    if (!empty($_POST['email'])) {
+        $email = $_POST['email'];
+    }
+
+    if (!empty($_POST['mobile'])) {
+        $mobile = $_POST['mobile'];
+    }
+
+    if (!empty($_POST['college'])) {
+        $college = $_POST['college'];
+    }
+
+
+    include 'connection.php';
+
+    if ($conn) {
+
+        $sql = "SELECT max(part_id) FROM solosinging";
+        $retval = mysqli_query($conn, $sql);
+        if(!$retval)
+        {
+            die('could not get data:'.mysqli_error());
+        }
+
+        while($row=mysqli_fetch_array($retval,MYSQLI_ASSOC))
+        {
+            $id=$row['max(part_id)'];
+            $id=$id+1;
+
+        }
+
+        $event_id=$event_id.$id;
+
+        $sql = "insert into solosinging values('$name','$mobile','$email','$college','$id')";
+        if (mysqli_query($conn, $sql) == 1) {
+
+//            //sending email
+//            $email_subject = "ODYSSEY Registration";
+//            $email_body = "Thank you for registering in Codezilla.\nYour Participation ID is ".$event_id;
+//            mail($email, $email_subject, $email_body);
+
+            //sending sms
+//            include('way2sms-api.php');
+//            sendWay2SMS ( "8197508688","R3694K",$mobile,$email_body);
+
+            ?>
+            <script language="javascript" type="text/javascript">
+                alert('Registered successfully.Your ID is <?php echo $event_id;?>');
+                window.location = "reg_solosinging.html";
+            </script>
+            <?php
+            $conn->close();
+        } else {
+            ?>
+            <script language="javascript" type="text/javascript">
+                alert('You have already registered to this event.');
+                window.location = "reg_solosinging.html";
+            </script>
+            <?php
+        }
+    }
+} else {
+    echo "Sorry for inconvience, please try again later.";
+}
+
+
+?>
